fab1a5b04e235e1ecd70428355fc340a5ee9a36c
Arthur G. Bertoti <artgbertoti@gmail.com>
2023-02-11 11:50:34 -0300

    implementada a pesquisa na tabela

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -5,32 +5,86 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import { IconButton } from '@mui/material';
+import { IconButton, Input, InputAdornment, InputBase } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
 import InfoIcon from '@mui/icons-material/Info';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
-import Seacher from './Searcher';
+import Filter from './Filter';
+import * as React from 'react';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
 
-export default function TableComponent<T = unknown>(Header: string[], data: T[] | null | undefined, isFetching: boolean, error: any) {
+export default function TableComponent<T = unknown>(
+  header: string[],
+  data: T[] | null | undefined,
+  isFetching: boolean,
+  error: any
+  /*,
+  filterMenu: boolean,
+  filterData: [] | null | undefined*/
+) {
+
+  const [search, setSearch] = React.useState('');
+  const searchLabel = 'Pesquisar...';
+  const [filter, setFilter] = React.useState('');
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setFilter(event.target.value);
+  };
+
+  const handleSearch = (search: string) => {
+    setSearch(search);
+  };
+
+
+
+
+  const filteredData = data?.filter((item: any) => {
+    return Object.keys(item).some((key) =>
+      item[key].toString().toLowerCase().includes(search.toLowerCase())
+    );
+  });
 
   return (
     <TableContainer component={Paper}>
       <Table aria-label="simple table">
         <TableHead>
           <TableRow>
-            {Header.map((header: any) => (
-              <TableCell key={header} align={header === Header[0] ? 'left' : 'center'}
-               >{header}</TableCell>
-            ))}
-            <Seacher />
+            <>
+              {header.map((headerName: any) => (
+                <TableCell key={headerName} align={headerName === header[0] ? 'left' : 'center'}
+                >{headerName}</TableCell>
+              ))}
+              <TableCell>
+                {/* {filterMenu &&
+                { Filter(filterData: [] | null | undefined) { } }} */}
+                <FormControl variant="outlined">
+                  <Input
+                    placeholder="Pesquisar..."
+                    onChange={(e) => handleSearch(e.target.value)}
+                    value={search}
+                    inputProps={{ 'aria-label': 'search' }}
+                    endAdornment={
+                      <InputAdornment position="end">
+                        <SearchIcon color="inherit" fontSize="small" />
+                      </InputAdornment>
+                    }
+                  />
+                </FormControl>
+              </TableCell>
+            </>
           </TableRow>
         </TableHead>
         <TableBody>
           {isFetching && <TableRow>Carregando...</TableRow>}
           {error && <TableRow>Erro ao carregar os produtos</TableRow>}
-          {data?.map((item: any) => (
+          {filteredData?.map((item: any) => (
+
             <TableRow key={item.id}>
+
               {Object.keys(item).map((key: any) => (
+
                 <TableCell key={key}
                   align={key === Object.keys(item)[0] ? 'left' : 'center'}
                 >{item[key]}</TableCell>
--- src/pages/Products/index.tsx
+++ src/pages/Products/index.tsx
@@ -1,14 +1,17 @@
 import TableComponent from '../../components/Table';
 import { useFetch } from '../../hooks/useFetch';
 import IProduct from '../../interfaces/IProduct';
+import ICategory from '../../interfaces/ICategory';
 import { Button } from '@mui/material';
 
 export default function Products() {
     const { data: products, error, isFetching } = useFetch<IProduct[]>('products');
+    const { data: categories } = useFetch<ICategory[]>('categories');
     const headers = ["Produto", "Código", "Estoque", "Valor un", "categoryName"];
+
     const bodyData = products?.map(product => {
         return {
-            product: product.name,
+            name: product.name,
             code: product.description,
             storage: 100,
             priceUnit: product.barcode,
@@ -19,7 +22,14 @@ export default function Products() {
     return (
         <section>
             <h1>Todos os produtos</h1>
-            {TableComponent(headers, bodyData, isFetching, error)}
+            {TableComponent(
+                headers,
+                bodyData,
+                isFetching,
+                error/*,
+                true,
+                filterData*/
+                )}
             <Button variant="outlined">
                 Novo produto
             </Button>
